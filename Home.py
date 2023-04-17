43f9d88bdf1af42d75aec887b6e63664922a11eb
manelreghima <manelreghima@gmail.com>
2023-04-18 00:50:35 +0100

    'Home.py'

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ st.write("""
 # Explore Different Results
 """)
 
-st.sidebar.image("landex.png", use_column_width=True)
+st.sidebar.image("data/landex.png", use_column_width=True)
 
 dataset_name=st.sidebar.selectbox("Select Dataset",("Estonia","France"))
 land_type=st.sidebar.selectbox("Select type of land",("Forest land","Farmland",'Forest land and Farmland'))
@@ -30,7 +30,7 @@ model_name=st.sidebar.selectbox("Select Model",("Prophet","Linear Regression"))
 
 def get_model(model_name,dataset_name,land_type):
     if model_name=='Prophet' and dataset_name=='Estonia' and land_type=='Forest land':
-        df_est_forest = pd.read_csv('forest_land_estonia.csv')
+        df_est_forest = pd.read_csv('data/forest_land_estonia.csv')
         df_est_forest[['ds', 'y']] = df_est_forest[['year', 'avg_price_eur']]
         df_est_forest = df_est_forest[['ds', 'y']]
         m = Prophet()
@@ -45,7 +45,7 @@ def get_model(model_name,dataset_name,land_type):
         return(st.plotly_chart(fig, use_container_width=True))
     
     elif model_name=='Prophet' and dataset_name=='Estonia' and land_type=='Farmland':
-        df_est_farm = pd.read_csv('farmland_estonia.csv')
+        df_est_farm = pd.read_csv('data/farmland_estonia.csv')
         df_est_farm[['ds', 'y']] = df_est_farm[['year', 'avg_price_eur']]
         df_est_farm = df_est_farm[['ds', 'y']]
         
@@ -62,7 +62,7 @@ def get_model(model_name,dataset_name,land_type):
 
 
     elif model_name=='Linear Regression' and dataset_name=='Estonia' and land_type=='Forest land and Farmland':
-        df = pd.read_csv('maaamet_farm_forest_2022.csv')
+        df = pd.read_csv('data/maaamet_farm_forest_2022.csv')
         X = df[['year',  'number', 'average_area',
        'total_volume_eur', 'price_min', 'price_max', 'price_per_unit_min',
        'price_per_unit_max', 'price_per_unit_median',
@@ -76,7 +76,7 @@ def get_model(model_name,dataset_name,land_type):
         return(st.plotly_chart(fig, use_container_width=True))
     
     elif model_name=='Linear Regression' and dataset_name=='France' and land_type=='Forest land and Farmland':
-        df = pd.read_csv('farm_forest_france.csv')
+        df = pd.read_csv('data/farm_forest_france.csv')
         df.dropna(inplace=True)
         df = pd.get_dummies(df, columns=['land_type'])
         X = df.drop('price', axis=1)
@@ -93,7 +93,7 @@ def get_model(model_name,dataset_name,land_type):
 
 
     elif model_name=='Prophet' and dataset_name=='France' and land_type=='Forest land':
-        df_fra_forest = pd.read_csv('forest_land_france.csv',sep=';')
+        df_fra_forest = pd.read_csv('data/forest_land_france.csv',sep=';')
         df_fra_forest=df_fra_forest[df_fra_forest['country']=='FRA']
         df_fra_forest = df_fra_forest.drop(['indicator', 'Country name', 'country'], axis=1)
         df_fra_forest = df_fra_forest.rename(columns={'Indicator name': 'land_type', 'time': 'year', 'value': 'price'})
@@ -115,7 +115,7 @@ def get_model(model_name,dataset_name,land_type):
     
     
     elif model_name=='Prophet' and dataset_name=='France' and land_type=='Farmland':
-        df_fra_farm = pd.read_csv('farmland_france.csv',sep=';')
+        df_fra_farm = pd.read_csv('data/farmland_france.csv',sep=';')
         df_fra_farm=df_fra_farm[df_fra_farm['country']=='FRA']
         df_fra_farm = df_fra_farm.drop(['indicator', 'Country name', 'country'], axis=1)
         df_fra_farm = df_fra_farm.rename(columns={'Indicator name': 'land_type', 'time': 'year', 'value': 'price'})
